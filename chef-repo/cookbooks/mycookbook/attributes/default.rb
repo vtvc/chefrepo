08258bc6674a0f2d34d23777d9b82f57d4bd1983
devops <devops@gmail.com>
2019-01-28 09:27:23 +0530

    sdjfkls

--- chef-repo/cookbooks/mycookbook/attributes/default.rb
+++ chef-repo/cookbooks/mycookbook/attributes/default.rb
@@ -1,2 +1,15 @@
-default['mycookbook']['mywebattribue']='apache2'
+
 default['mycookbook']['mygitattribue']='git'
+
+if node['platform'] == 'ubuntu'
+default['mycookbook']['mywebattribue']='apache2'
+end
+
+
+
+if node['platform'] =='redhat'
+default['mycookbook']['mywebattribue']='httpd'
+end
+
+
+default['platform']='maha'
--- chef-repo/cookbooks/mycookbook/metadata.rb
+++ chef-repo/cookbooks/mycookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures mycookbook'
 long_description 'Installs/Configures mycookbook'
-version '0.1.10'
+version '0.1.12'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
