5125beb23f9295fe241f243abcb0979f08f24f9b
maha <maha@gmail.com>
2019-10-24 09:38:51 +0530

    tom8

---
+++ chef-repo/cookbooks/mytom8onu16/recipes/tomstart.rb
@@ -0,0 +1,25 @@
+execute 'name' do
+    command 'sudo systemctl daemon-reload'
+    action :run
+end
+
+execute 'name' do
+    command 'sudo systemctl start tomcat'
+    action :run
+    not_if{File.exist?('/home/ubuntu/tomstart')}
+end
+
+execute 'name' do
+    command 'sudo systemctl restart tomcat'
+    action :run
+    only_if{File.exist?('/home/ubuntu/tomstart')}
+end
+
+
+file '/home/ubuntu/tomstart' do
+    content 'start'
+    mode '0755'
+    action :create
+end
+
+
